0eb3b9fd3104cf790d3e92af460da6beecc82129
Abhi <suriabhinav1997@gmail.com>
2016-12-07 23:39:51 -0500

    integrated ajax upload

--- app/account/views.py
+++ app/account/views.py
@@ -2,7 +2,7 @@ from flask import flash, redirect, render_template, request, url_for
 from flask.ext.login import (current_user, login_required, login_user,
                              logout_user)
 from flask.ext.rq import get_queue
-
+import time
 import os, json, boto3
 from . import account
 from .. import db
@@ -258,11 +258,12 @@ def sign_s3():
   S3_BUCKET = os.environ.get('S3_BUCKET')
 
   # Load required data from the request
-  file_name = request.args.get('file-name')
+  pre_file_name = request.args.get('file-name') 
+  file_name = ''.join(pre_file_name.split('.')[:-1]) + str(time.time()).replace('.','-') + '.' + ''.join(pre_file_name.split('.')[-1:])
   file_type = request.args.get('file-type')
 
   # Initialise the S3 client
-  s3 = boto3.client('s3')
+  s3 = boto3.client('s3', 'us-west-2')
 
   # Generate and return the presigned URL
   presigned_post = s3.generate_presigned_post(
@@ -273,7 +274,7 @@ def sign_s3():
       {"acl": "public-read"},
       {"Content-Type": file_type}
     ],
-    ExpiresIn = 3600
+    ExpiresIn = 6000
   )
 
   print "presigned_post: "
@@ -281,6 +282,7 @@ def sign_s3():
   # Return the data to the client
   return json.dumps({
     'data': presigned_post,
-    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
+    'url_upload': 'https://%s.s3-us-west-2.amazonaws.com' % (S3_BUCKET),
+    'url': 'https://%s.s3-us-west-2.amazonaws.com/%s' % (S3_BUCKET, file_name)
   })
 
