62a035d7f6204edb281d472067d6ce1470712876
Abhi <suriabhinav1997@gmail.com>
2016-12-10 03:34:52 -0500

    file upload progress

--- app/account/views.py
+++ app/account/views.py
@@ -257,7 +257,7 @@ def sign_s3():
   # Load necessary information into the application
   S3_BUCKET = os.environ.get('S3_BUCKET')
   S3_REGION = os.environ.get('S3_REGION')
-  TARGET_FOLDER = 'json/'
+  TARGET_FOLDER = 'json'
   # Load required data from the request
   pre_file_name = request.args.get('file-name') 
   file_name = ''.join(pre_file_name.split('.')[:-1]) + str(time.time()).replace('.','-') + '.' + ''.join(pre_file_name.split('.')[-1:])
@@ -269,7 +269,7 @@ def sign_s3():
   # Generate and return the presigned URL
   presigned_post = s3.generate_presigned_post(
     Bucket = S3_BUCKET,
-    Key = TARGET_FOLDER + file_name,
+    Key = TARGET_FOLDER + "/" + file_name,
     Fields = {"acl": "public-read", "Content-Type": file_type},
     Conditions = [
       {"acl": "public-read"},
@@ -284,6 +284,6 @@ def sign_s3():
   return json.dumps({
     'data': presigned_post,
     'url_upload': 'https://%s.%s.amazonaws.com' % (S3_BUCKET, S3_REGION),
-    'url': 'https://%s.%s.amazonaws.com/%s' % (S3_BUCKET, S3_REGION, file_name)
+    'url': 'https://%s.%s.amazonaws.com/%s/%s' % (S3_BUCKET, S3_REGION, TARGET_FOLDER, file_name)
   })
 
