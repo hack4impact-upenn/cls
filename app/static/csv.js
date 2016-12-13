abac9beade5d981e04b1c9bd23029ce2634e1854
Katie Jiang <kajiang@seas.upenn.edu>
2016-12-13 00:18:03 -0500

    separating js

---
+++ app/static/csv.js
@@ -0,0 +1,180 @@
+// INIT CSV
+var csv = [
+  ["name of area", "end time", "duration"]
+];
+weekStartMS = 0;
+weekEndMS = 0;
+
+// On click of submit button start csv generation process
+$('#submitProcess').on('click', function (e) {
+  var errString = "";
+  $('#submitErrors').remove();
+  // if (!uploadURL) {
+  //   errString += "No file has been uploaded. Please select a file.";
+  // }
+  if (Object.keys(boxes).length <= 0) {
+    errString += " Please select at least one bounding box."
+  }
+  if (!$('#filename').val()) {
+    errString += " Please enter a name for your file."
+  }
+  if ($("#startDay").val() === "") {
+    errString += " Please select a valid start day"
+  }
+  if ($("#endDay").val() === "") {
+    errString += " Please select a valid end day."
+  }
+
+  if (errString.length > 0) {
+    $('#submitProcess').before("<div id='submitErrors' class='col-md-12'><div class='alert alert-danger'>" + errString + "<\/div><\/div>");
+  } else {
+    obj = processData(uploadURL, boxes, $('#email').val(), $('#startDay').val(), $('#endDay').val());
+    inside = new Array(obj.boxes.length).fill(false);
+    startTimes = new Array(obj.boxes.length).fill(null);
+    durations = new Array(obj.boxes.length).fill(0);
+    var newOboe = oboe();
+
+    newOboe.node('locations.*', function (location) {
+      if (weekEndMS === 0) {
+        weekEndMS = moment.unix(location.timestampMs / 1000).day(obj.dayEnd).hour(23).minute(59).unix() * 1000;
+        weekStartMS = moment.unix(location.timestampMs / 1000).day(obj.dayStart).hour(0).minute(0).unix() * 1000;
+      }
+
+      if (location.timestampMs >= weekStartMS && location.timestampMs <= weekEndMS) {
+        // within a week
+        for (var i = 0; i < obj.boxes.length; i++) {
+          // iterate thru the boxes
+          var box = obj.boxes[i];
+          var lat = location.latitudeE7 / Math.pow(10, 7)
+          var lng = location.longitudeE7 / Math.pow(10, 7)
+            // check if in box coors && not inside a box
+          if (box['swLat'] <= lat && box['neLat'] >= lat &&
+            box['swLng'] <= lng && box['neLng'] >= lng && !inside[i]) {
+            console.log('enetered ' + box.name);
+            inside[i] = true;
+            startTimes[i] = location.timestampMs;
+          } else if ((box['swLat'] <= lat && box['neLat'] >= lat &&
+              box['swLng'] <= lng && box['neLng'] >= lng) && !inside[i]) {
+            durations[i] = Math.abs(location.timestampMs - startTimes[i]);
+          }
+          // not inside the box
+          else if (!(box['swLat'] <= lat && box['neLat'] >= lat &&
+              box['swLng'] <= lng && box['neLng'] >= lng) && inside[i]) {
+            inside[i] = false;
+            durations[i] = Math.abs(location.timestampMs - startTimes[i]);
+          }
+        }
+
+      }
+      else if (location.timestampMs < weekStartMS) {
+        console.log(moment.unix(location.timestampMs / 1000).format('MM-DD-YYYY ddd HH:mm'));
+        weekEndMS -= 604800000
+        weekStartMS -= 604800000
+        var locationtotal = 0;
+        for (var i = 0; i < obj.boxes.length; i++) {
+          if (startTimes[i] && durations[i] > 0) {
+            console.log('pushed!' + startTimes[i]);
+            csv.push([obj.boxes[i].name, moment.unix(startTimes[i] / 1000).format('MM-DD-YYYY ddd HH:mm'),
+              (durations[i] / 1000 / 3600).toFixed(2) + " hrs"
+            ])
+            startTimes[i] = weekEndMS;
+            locationtotal += (durations[i] / 1000 / 3600).toFixed(2)
+          }
+        }
+        if (locationtotal > 0) {
+          csv.push([], ["Total time this week is: " + locationtotal], [])
+        }
+
+      }
+      return oboe.drop;
+    }).done('done');
+    parseFileToCSV($('#uploadInput').get(0).files[0], newOboe)
+  }
+});
+
+function processData(uploadURL, boxes, email, start, end) {
+  var url = uploadURL;
+  var boxKeys = Object.keys(boxes);
+  var resBoxArray = [];
+  for (var i = 0; i < boxKeys.length; i++) {
+    var parseObj = boxes[boxKeys[i]];
+    var bounds = parseObj.rectangle._bounds;
+    var ne = bounds._northEast;
+    var sw = bounds._southWest;
+    var neLat = ne.lat;
+    var neLng = ne.lng;
+    var swLat = sw.lat;
+    var swLng = sw.lng;
+    resBoxArray.push({
+      name: parseObj.name,
+      neLat: neLat,
+      neLng: neLng,
+      swLat: swLat,
+      swLng: swLng
+    });
+  }
+  return {
+    url: url,
+    boxes: resBoxArray,
+    email: email,
+    dayStart: start,
+    dayEnd: end
+  }
+}
+
+function parseFileToCSV(file, oboeInstance) {
+  var fileSize = file.size;
+  var chunkSize = 512 * 1024; // bytes
+  var offset = 0;
+  var self = this; // we need a reference to the current object
+  var chunkReaderBlock = null;
+  var startTime = Date.now();
+  var endTime = Date.now();
+  var readEventHandler = function (evt) {
+    if (evt.target.error == null) {
+      offset += evt.target.result.length;
+      var progress = (100 * offset / fileSize).toFixed(2);
+      var trimmed = (100 * offset / fileSize).toFixed(0);
+      $("#done").css('width', trimmed + '%').attr('aria-valuenow', trimmed).text(progress + '%')
+        // console.log(progress);
+      var chunk = evt.target.result;
+      oboeInstance.emit('data', chunk); // callback for handling read chunk
+    } else {
+      console.log("Read error: " + evt.target.error);
+      return;
+    }
+    if (offset >= fileSize) {
+      oboeInstance.emit('done');
+      console.log("Done reading file");
+      endTime = Date.now();
+      // $("#stats").text("Time taken: " + ((endTime - startTime) / 1000).toFixed(2) +
+      //   "s for file size " + (fileSize / (1024 * 1024)).toFixed(2) + " MB")
+      // lv.ProcessView()
+      var csvContent = "data:text/csv;charset=utf-8,";
+      csv.forEach(function (infoArray, index) {
+        dataString = infoArray.join(",");
+        csvContent += index < csv.length ? dataString + "\n" : dataString;
+      });
+      var encodedUri = encodeURI(csvContent);
+      var link = document.createElement("a");
+      link.setAttribute("href", encodedUri);
+      link.setAttribute("download", "" + $("#filename").val() + ".csv");
+      document.body.appendChild(link);
+      link.click()
+      return;
+    }
+
+    // of to the next chunk
+    chunkReaderBlock(offset, chunkSize, file);
+  }
+
+  chunkReaderBlock = function (_offset, length, _file) {
+    var r = new FileReader();
+    var blob = _file.slice(_offset, length + _offset);
+    r.onload = readEventHandler;
+    r.readAsText(blob);
+  }
+
+  // now let's start the read with the first block
+  chunkReaderBlock(offset, chunkSize, file);
+}
---
+++ app/static/map.js
@@ -0,0 +1,151 @@
+/* INITIALIZE MAP */
+var mymap = L.map('mapid', {
+  scrollWheelZoom: false,
+  zoomControl: false
+}).setView([39.9526, -75.163], 3);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWJoaXN1cmk5NyIsImEiOiJjaXc0MHcyZGkwMndmMnRvMHplM3c4cmV0In0.NIVRBOjWLn8rVRR9EgDQRw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
+}).addTo(mymap);
+L.control.zoom({
+  position: 'bottomright'
+}).addTo(mymap);
+$("#cancel").prop('disabled', true);
+
+/* MAP STATE */
+var newBoxMode = false;
+var bounds = [];
+var boxes = {};
+var currentBox;
+var clicking = false;
+var dragStart, dragEnd;
+var uploadURL;
+
+/* MOUSE DRAG -> MAP BOX FUNCTIONALITY */
+
+function mousedownMap(e) {
+  if (newBoxMode) {
+    clicking = true;
+    bounds[0] = e.latlng;
+    bounds[1] = e.latlng;
+    currentBox = L.rectangle(bounds, {
+      color: 'blue',
+      weight: 1
+    });
+  }
+}
+
+function mousemoveMap(e) {
+  if (newBoxMode && clicking) {
+    bounds[1] = e.latlng;
+    currentBox.setBounds(bounds);
+    currentBox.addTo(mymap);
+  }
+}
+
+
+/* HANDLE MAKING CHANGES TO THE TABLE FOR SHOWING BOXES */
+function createBox(name) {
+  if (name) {
+    boxes[currentBox._leaflet_id] = {
+      'name': name,
+      'rectangle': currentBox,
+      'zoomLevel': mymap.getZoom()
+    };
+
+
+    $('#locationTable tr:last').after(
+      '<tr id="' + currentBox._leaflet_id + '"><td>' + name +
+      '<\/td><td><button id="goToButton' + currentBox._leaflet_id + '" data-box-id="' + currentBox._leaflet_id + '" class="btn btn-default">Go To Box<\/button>&nbsp;<button class="btn btn-danger deleteBox">Delete<\/button><\/td><\/tr>'
+    );
+
+    $('#goToButton' + currentBox._leaflet_id).on('click', function (e) {
+      var obj = boxes[$(this).data('boxId')]
+      console.log(obj);
+      var bounds = obj.rectangle._bounds;
+      var neLat = bounds._northEast.lat
+      var neLon = bounds._northEast.lng
+      var swLat = bounds._southWest.lat
+      var swLon = bounds._southWest.lng
+      var lat = swLat + ((neLat - swLat) / 2);
+      var long = swLon - ((neLon - swLon) / 2);
+      mymap.setView(L.latLng(lat, long), obj.zoomLevel);
+    })
+
+    $('.deleteBox').on('click', function (e) {
+      var id = this.parentNode.parentNode.id;
+      boxes[id].rectangle.editing.disable();
+      boxes[id].rectangle.remove();
+      delete boxes[id];
+      this.parentNode.parentNode.remove();
+    });
+
+    currentBox.bindPopup(name).openPopup();
+    currentBox.editing.enable();
+  } else {
+    currentBox.remove();
+  }
+  currentBox = null;
+}
+
+/* RENDERING OF BOXES AT DIFFERENT ZOOM LEVELS */
+function toggleAllBoxMarkers(e) {
+  var boxKeys = Object.keys(boxes);
+  for (var i = 0; i < boxKeys.length; i++) {
+    toggleBoxMarker(boxes[boxKeys[i]], i);
+  }
+}
+
+function toggleBoxMarker(box, i) {
+  var rect = box.rectangle;
+  var zoom = box.zoomLevel;
+  var boxPane = rect.getPane().firstChild.firstChild.childNodes[i];
+
+  var w = boxPane.getBoundingClientRect().width;
+  var h = boxPane.getBoundingClientRect().height;
+
+  if ((w > 40 && h > 40) || mymap.getZoom() >= box.zoomLevel) {
+    rect.editing.enable();
+  } else {
+    rect.editing.disable();
+  }
+}
+
+
+function mouseupMap(e) {
+  if (newBoxMode && currentBox) {
+    bounds[1] = e.latlng;
+    currentBox.setBounds(bounds);
+    currentBox.addTo(mymap);
+    boxPane = currentBox.getPane();
+
+    bootbox.prompt('Name of location', createBox);
+
+    mymap.dragging.enable();
+    newBoxMode = false;
+    clicking = false;
+    $("#cancel").prop('disabled', true);
+    $("#newBoxButton").prop('disabled', false);
+  }
+}
+
+mymap.on('mousedown', mousedownMap);
+mymap.on('mousemove', mousemoveMap);
+mymap.on('mouseup', mouseupMap);
+mymap.on('zoom', toggleAllBoxMarkers)
+
+$('#newBoxButton').on('click', function (e) {
+  newBoxMode = true;
+  $('#newBoxButton').prop('disabled', true);
+  $('#cancel').prop('disabled', false);
+  mymap.dragging.disable();
+})
+$('#cancel').on('click', function (e) {
+  newBoxMode = false;
+  $("#newBoxButton").prop('disabled', false);
+  $("#cancel").prop('disabled', true);
+  mymap.dragging.enable();
+})
